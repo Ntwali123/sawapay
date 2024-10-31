2339680576530ea91ed9febeeaca50e98caf8fd9
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2024-10-31 18:45:49 +0200

    changes made

---
+++ app/navbar/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const page = () => {
+  return (
+<nav>
+  <p >SawaPay</p>
+</nav>
+  )
+}
+
+export default page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
-import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    colors: {
+      "light-green":"89C550",
+      "light-gray":"E8EDF2",
+      "word-color":"#111111",
+      "background":"F1F9FF",
+
     },
+    fontFamily: {
+      dm: ['DM Sans', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+}
